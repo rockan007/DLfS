1d3208ae9378fda4098badfdc81f271abce46016
Andy An <rockan007@gmail.com>
2023-11-01 04:51:08 +0000

    输出层的设计&手写数字识别

---
+++ chapter_3_neural_networks/mnist_show.py
@@ -0,0 +1,23 @@
+import sys,os
+sys.path.append(os.pardir)
+from dataset.mnist import load_mnist
+from PIL import Image
+import numpy as np
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    #pil_img.show()
+    #Jupyter 显示图片
+    display(pil_img)
+
+# (训练图像，训练标签),(测试图像，测试标签)
+(x_train,t_train),(x_test,t_test) = load_mnist(flatten=True,
+normalize=False)
+img = x_train[0]
+label = t_train[0]
+print(label)
+
+print(img.shape)
+img = img.reshape(28,28)
+print(img.shape)
+img_show(img)
